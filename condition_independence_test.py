2a6d2a2dba67b0d3a471dfcc7b4cddced6cae686
Miao-jiaLi <83415666+Miao-jiaLi@users.noreply.github.com>
2024-12-30 17:03:31 +0800

    CG-generater

---
+++ condition_independence_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+ @Time    : 2019/11/13 21:34
+ @File    : independence_condition_test.py
+ """
+import pdb
+
+# from ..common.chi_square_test import chi_square_test
+from P_C.common.fisher_z_test import cond_indep_fisher_z
+# from ..common.chi_square_test import chi_square
+from P_C.common.g2test import g2_test_dis
+import pdb
+
+def cond_indep_test(data, target, var, cond_set=[], is_discrete=True, alpha=0.05):
+    if is_discrete:
+        pval, dep = g2_test_dis(data, target, var, cond_set,alpha)
+        # if selected:
+        #     _, pval, _, dep = chi_square_test(data, target, var, cond_set, alpha)
+        # else:
+        # _, _, dep, pval = chi_square(target, var, cond_set, data, alpha)
+    else:
+        CI, dep = cond_indep_fisher_z(data, target, var, cond_set, alpha)
+        #pdb.set_trace()
+    return CI, dep
